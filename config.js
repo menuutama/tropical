6f539daca32edf0205aa5f9b0a9dcc3f0fa2c4d4
menuutama <rani.yong203@gmail.com>
2026-05-29 03:29:00 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbwyHlDqGumNenEhW6w5iAcA2984E1AbnXOfemzaxPOgk8pqXKD-pg6zw4Rw6U3sk-tY/exec";
+
+const CLOUDINARY_CONFIG = {
+  cloudName: 'dwtcmpzez',
+  apiKey: '958334142538248'
+};
