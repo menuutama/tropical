b3dfb8e1d59160ca6e8cc18ef2c2638c1a6d052e
menuutama <rani.yong203@gmail.com>
2026-05-31 01:42:38 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,4 @@
 const export API_URL = "https://script.google.com/macros/s/AKfycbzfe5rC_LbWSLT5uNKsA5CdLttBtcab6ALvJ1hRTFlOMaechmmx-IxW2X4NdqjUkyo/exec";
+export function ambilData() {
+  console.log("Mengambil data...");
+}
