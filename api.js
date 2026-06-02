fbf9abcc58c4b75115505cf80f93d184ca6f01bc
menuutama <rani.yong203@gmail.com>
2026-06-02 22:25:02 +0800

    Delete api.js

--- api.js
+++
@@ -1,4 +0,0 @@
-export const  API_URL = "https://script.google.com/macros/s/AKfycbzfe5rC_LbWSLT5uNKsA5CdLttBtcab6ALvJ1hRTFlOMaechmmx-IxW2X4NdqjUkyo/exec";
-export function loadData() {
-  console.log("Mengambil data...");
-}
